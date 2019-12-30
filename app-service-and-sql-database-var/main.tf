de2c0604ea5cab90b0dcb88078b0be2d96ac453c
Fredy Alexander Benavides Ocampo <alexander.benavides@globant.com>
2019-12-30 12:32:35 -0500

    including configuration

--- app-service-and-sql-database-var/main.tf
+++ app-service-and-sql-database-var/main.tf
@@ -1,10 +1,9 @@
 terraform {
   required_version = ">= 0.11"
   backend "azurerm" {
-    storage_account_name = "__terraformstorageaccount__"
-    container_name        = "terraform"
+    storage_account_name = "tstate6103"
+    container_name        = "tstate"
     key                   = "terraform.tfstate"
-  access_key    ="__storagekey__"  
   }
 }
 
